ac2c614fdf292fca87b50e1df70382322c74b2d3
Wilson Diaz <wdfiguereo@gmail.com>
2021-06-16 21:33:29 -0400

    4.4-5: totalLikes and favoriteBlog

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,6 +1,26 @@
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => 1
 
+const totalLikes = (blogs) => {
+  if (!blogs || blogs.length === 0) { return 0 }
+  else if (blogs.length === 1) { return blogs[0].likes }
+
+  // more than one blog
+  return blogs.reduce((sum, blog) => {
+    return sum + blog.likes
+  }, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  if (!blogs || blogs.length === 0) { return null }
+  else if (blogs.length === 1) { return blogs[0] }
+
+  const maxLikes = Math.max(...blogs.map(x => x.likes))
+  return blogs.find(x => x.likes === maxLikes)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
