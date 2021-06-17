58e19ff9283fb3e9930d7fe09b6e9c1d6044eff9
Wilson Diaz <wdfiguereo@gmail.com>
2021-06-16 20:31:37 -0400

    4.2: refactor into modules

---
+++ app.js
@@ -0,0 +1,18 @@
+const config = require('./utils/config')
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+console.log('connecting to mongodb')
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => console.log('connected successfully'))
+  .catch((error) => console.log('error connecting:', error.message))
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
