c4f016f747d3a9c07015184135c76528e842ab1d
Wilson Diaz <wdfiguereo@gmail.com>
2021-07-20 10:29:13 -0400

    4.13: use describe blocks

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -14,78 +14,82 @@ beforeEach(async () => {
   await Promise.all(promises)
 })
 
-test('all blogs returned as json', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('viewing initial blogs saved', () => {
+  test('all blogs returned as json', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-}, 100000)
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  }, 100000)
 
-test('blogs have unique identifier named id', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+  test('blogs have unique identifier named id', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
 
-  expect(response.body[0].id).toBeDefined()
+    expect(response.body[0].id).toBeDefined()
+  })
 })
 
-test('post is successful', async () => {
-  const newBlog = {
-    title: 'thisisatestblog',
-    author: 'Test Author',
-    url: 'https://myurl.test',
-    likes: 3
-  }
+describe('adding a new blog', () => {
+  test('post is successful', async () => {
+    const newBlog = {
+      title: 'thisisatestblog',
+      author: 'Test Author',
+      url: 'https://myurl.test',
+      likes: 3
+    }
 
-  const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  // check if stored in DB
-  const blogsAtEnd = await helper.blogsInDB()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    // check if stored in DB
+    const blogsAtEnd = await helper.blogsInDB()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
-  // check data
-  delete response.body.id
-  expect(response.body).toEqual(newBlog)
-})
+    // check data
+    delete response.body.id
+    expect(response.body).toEqual(newBlog)
+  })
 
-test('missing likes default to 0', async () => {
-  const newBlog = {
-    title: 'thisisatestblog',
-    author: 'Test Author',
-    url: 'https://myurl.test',
-  }
+  test('missing likes default to 0', async () => {
+    const newBlog = {
+      title: 'thisisatestblog',
+      author: 'Test Author',
+      url: 'https://myurl.test',
+    }
 
-  const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  // check likes
-  expect(response.body.likes).toBe(0)
-})
+    // check likes
+    expect(response.body.likes).toBe(0)
+  })
 
-test('missing title and url cause error', async () => {
-  const newBlog = {
-    author: 'Test Author',
-    likes: 9
-  }
+  test('missing title and url cause error', async () => {
+    const newBlog = {
+      author: 'Test Author',
+      likes: 9
+    }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
 
-  // check that it wasn't added
-  const blogsAtEnd = await helper.blogsInDB()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    // check that it wasn't added
+    const blogsAtEnd = await helper.blogsInDB()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
 afterAll(() => {
