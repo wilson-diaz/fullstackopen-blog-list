367f56eb549656f71507afd58583198da676038b
wilson-diaz <wdfiguereo@gmail.com>
2021-08-27 20:29:18 -0400

    e2e testing reqs

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -41,7 +41,7 @@ blogsRouter.post('/', async (request, response) => {
   const savedBlog = await blog.save()
 
   user.blogs = user.blogs.concat(savedBlog._id)
-  user.save()
+  await user.save()
 
   response.status(201).json(savedBlog)
 })
